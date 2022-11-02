a8d20be020bc026540d11da67509f904438e8fdc
Ramy Ouda <ramyouda@gmail.com>
2022-11-02 12:02:26 -0400

    Create getStories method

--- src/app/hos-api.service.ts
+++ src/app/hos-api.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HosApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  url = 'https://hack-or-snooze-v3.herokuapp.com/';
+
+  getStories() {
+    this.http.get<any>(this.url + 'stories');
+  }
 }
