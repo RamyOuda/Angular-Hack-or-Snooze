35cc1299b125bf5ccdec8e2fc8f429715ce7093b
Ramy Ouda <ramyouda@gmail.com>
2022-11-02 12:06:31 -0400

    Get data from API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HosApiService } from '../hos-api.service';
 
 @Component({
-  selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private apiService: HosApiService) {}
 
-  constructor() { }
+  data$: any;
 
   ngOnInit(): void {
+    this.data$ = this.apiService.getStories();
   }
-
 }
