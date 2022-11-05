1284398f2904db686ebb307e1a649cdea625d3aa
Ramy Ouda <ramyouda@gmail.com>
2022-11-05 12:56:34 -0400

    Remove RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
@@ -12,7 +11,6 @@ import { HomeComponent } from './home/home.component';
   declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    RouterModule,
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
