30f477157bc6ab25739ce8b95bab58632e97eb4d
Ramy Ouda <ramyouda@gmail.com>
2022-11-07 13:57:02 -0500

    Refactor stories$ (Mentor)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { HosApiService } from '../api/hos-api.service';
-import { Stories } from './stories';
+import { Story, StoryData } from './stories';
 
 import { Store } from '@ngrx/store';
-import { testAction } from '../state/test/test.actions';
 
 @Component({
   templateUrl: './home.component.html',
@@ -13,22 +12,14 @@ import { testAction } from '../state/test/test.actions';
 export class HomeComponent implements OnInit {
   constructor(private store: Store, private apiService: HosApiService) {}
 
-  stories$: Observable<Stories[]> = this.apiService.getStories$.pipe(
-    map((data: any) => {
-      this.shortUrls = data.stories.map(
-        (story: any) => new URL(story.url).hostname
-      );
-      return data.stories;
-    })
+  stories$: Observable<Story[]> = this.apiService.getStories$.pipe(
+    map((data: any) =>
+      data.stories.map((story: Story) => ({
+        ...story,
+        shortUrl: new URL(story.url).hostname,
+      }))
+    )
   );
 
-  shortUrls!: string[];
-
-  ngOnInit(): void {
-    this.runTest();
-  }
-
-  runTest() {
-    this.store.dispatch(testAction());
-  }
+  ngOnInit(): void {}
 }
