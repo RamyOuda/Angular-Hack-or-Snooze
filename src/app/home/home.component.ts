0ffcb5e0bbfe9e8bd401853768d2478a02f4c40a
Ramy Ouda <ramyouda@gmail.com>
2022-11-07 11:29:02 -0500

    Impliment NgRx test (WIP)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,15 @@ import { map, Observable } from 'rxjs';
 import { HosApiService } from '../api/hos-api.service';
 import { Stories } from './stories';
 
+import { Store } from '@ngrx/store';
+import { testAction } from '../state/test/test.actions';
+
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private apiService: HosApiService) {}
+  constructor(private store: Store, private apiService: HosApiService) {}
 
   stories$: Observable<Stories[]> = this.apiService.getStories$.pipe(
     map((data: any) => {
@@ -21,5 +24,11 @@ export class HomeComponent implements OnInit {
 
   shortUrls!: string[];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.runTest();
+  }
+
+  runTest() {
+    this.store.dispatch(testAction());
+  }
 }
